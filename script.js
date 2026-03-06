4f7d60cca4efd9967d753e712f4e4acfd73c69e7
kuraymd <workrun0903@gmail.com>
2026-03-06 17:10:10 +0900

    Add async functions to fetch and display images

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+async function getImage(name){
+
+const url = `https://ja.wikipedia.org/api/rest_v1/page/summary/${name}`;
+
+const res = await fetch(url);
+const data = await res.json();
+
+return data.thumbnail ? data.thumbnail.source : null;
+
+}
+
+async function generate(){
+
+const creature = creatures[Math.floor(Math.random()*creatures.length)];
+const motif = motifs[Math.floor(Math.random()*motifs.length)];
+
+document.getElementById("name").textContent = creature.name;
+document.getElementById("desc").textContent = creature.desc;
+document.getElementById("motif").textContent = motif;
+
+const img = await getImage(creature.wiki);
+
+if(img){
+document.getElementById("image").src = img;
+}
+
+}
